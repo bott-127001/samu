37b8b6051f6e154dd0e9327e391ab1e249174aca
bott-127001 <samsalgar44@gmail.com>
2025-04-01 16:09:11 +0530

    Update worker.js

--- public/worker.js
+++ public/worker.js
@@ -1,15 +1,21 @@
-javascript
-Copy
 // worker.js
 let fetchInterval;
+let changeInterval;
 
 self.onmessage = function(e) {
     if (e.data === 'start') {
-        // Only start 5-second fetch interval
+        // 5-second interval for data fetching
         fetchInterval = setInterval(() => {
             self.postMessage('fetch');
         }, 5000);
+        
+        // 15-minute interval for change calculations
+        changeInterval = setInterval(() => {
+            self.postMessage('calculateChange');
+        }, 900000); // 15 minutes in milliseconds
+        
     } else if (e.data === 'stop') {
         clearInterval(fetchInterval);
+        clearInterval(changeInterval);
     }
 };
