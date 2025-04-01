3f1ce1220cd3eaa8e561827a309bfc50c3745611
bott-127001 <samsalgar44@gmail.com>
2025-04-01 18:18:21 +0530

    Update worker.js

--- public/worker.js
+++ public/worker.js
@@ -1,12 +1,20 @@
 // worker.js
 let fetchInterval;
+let isPaused = false;
 
 self.onmessage = function(e) {
-    if (e.data === 'start') {
+    if (e.data === 'start' && !isPaused) {
+        clearInterval(fetchInterval);
         fetchInterval = setInterval(() => {
             self.postMessage('fetch');
         }, 5000);
     } else if (e.data === 'stop') {
         clearInterval(fetchInterval);
+    } else if (e.data === 'pause') {
+        isPaused = true;
+        clearInterval(fetchInterval);
+    } else if (e.data === 'resume') {
+        isPaused = false;
+        self.postMessage('start');
     }
 };
